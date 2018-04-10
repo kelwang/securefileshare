e7f963fe7e70634d430e5a4529c502e409260bcd
kelwang <sherkrainwang@gmail.com>
2018-04-10 11:07:11 -0700

    Create pages.go

---
+++ ui/pages.go
@@ -0,0 +1,21 @@
+package ui
+
+import ()
+
+// DownloadPage shows a list of items
+const DownloadPage = `
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Files Downloader</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.css">
+</head>
+<body>
+	<table>
+		<thead></thead>
+	</table>
+</body>
+</html>
+`
